85a9f631bad93c50fd844fccf4ae7ad194d9df9a
him <68465510+shangguanxian@users.noreply.github.com>
2021-07-08 18:26:22 +0800

    Add files via upload

---
+++ 队列的其他操作.py
@@ -0,0 +1,19 @@
+a = [1,2,3]
+a.reverse()
+print(a)
+
+print('------------')
+
+b = ['hello','taobo','baidu','hello']
+b.count('hello')
+c = b.count('hello')
+print(c)
+b.count('aili')
+v = b.count('aili')
+print(v)
+
+print('-------------')
+
+z = [5,6,9,8,7,4,1,2,6,3]
+z.sort()
+print(z)
