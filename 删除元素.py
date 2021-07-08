55f4c4704f59f3a5b4d68113a2de45367f445dc4
shangguanxian <68465510+shangguanxian@users.noreply.github.com>
2021-07-08 17:55:38 +0800

    Add files via upload

---
+++ 删除元素.py
@@ -0,0 +1,11 @@
+a = [1,2,3,4,5,6]
+print(a)
+a.pop()
+print(a)
+s = a.pop()
+print(s)
+#pop可以指定删除某个值，如果不指明默认的是最后一个元素
+b = ['ailibaba','baidu','tenxun']
+b.remove('baidu')
+print(b)
+#remove可以根据元素内容来进行删除
